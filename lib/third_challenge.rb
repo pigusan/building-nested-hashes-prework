48057388054073919902b245b5475d54ba8e83aa
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-27 19:31:03 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -16,7 +16,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         :name => "Lord Capulet",
@@ -36,5 +36,5 @@ def third_challenge
    }
   }
 
-  
+
 end
