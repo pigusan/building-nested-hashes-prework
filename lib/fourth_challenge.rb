bb6e0e46332b7a23e6be5a8d54e6a0334669ebea
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-27 19:36:19 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -13,6 +13,11 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
+        {
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
+        }
         #your hashes here!
       ]
    }
